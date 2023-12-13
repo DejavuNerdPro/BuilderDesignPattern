98cfac5b2bb9a32a2319fd0000518aa7792a4bd4
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2023-12-13 13:22:44 +0630

    Document

---
+++ Product.java
@@ -0,0 +1,24 @@
+public class Product {
+    private String part1;
+    private String part2;
+    // Other parts...
+
+    public void setPart1(String part1) {
+        this.part1 = part1;
+    }
+
+    public void setPart2(String part2) {
+        this.part2 = part2;
+    }
+
+    // Other setters...
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "part1='" + part1 + '\'' +
+                ", part2='" + part2 + '\'' +
+                // Other parts...
+                '}';
+    }
+}
